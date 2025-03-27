6218a3cad01483ea09b89656501ada673989fd72
theandreeei <vertru112211@gmail.com>
2025-03-27 18:20:27 +0200

    Added E, F and Docstrings

--- main.py
+++ main.py
@@ -2,26 +2,48 @@
 Робота: Мордач Андрій, 10-А, школа номер 23
 Повна версія (як модуль, де всі завдання) знаходиться на https://github.com/theandreeei/olympiad_tasks_2025
 '''
-from validations import validation_for_a, validation_for_b, validation_for_c, validation_for_d
+from validations import validation_for_a, validation_for_b, validation_for_c, validation_for_d, validation_for_e, validation_for_f
 
 
 def natural_numbers_count(n: int):
+    '''
+    Програма на входi приймає натуральне число n. Потрiбно вивести кiлькiсть нату-
+    ральних чисел менших n.
+
+    Аргументи: n: int - натуральне число.
+    '''
     # Validation
     validation_for_a(n)
+
     # Code
     return n - 1
 
 
-def splitted_texts_by_numbers(n: int, text:str, ignore_empty_text=False):
+def splitted_texts_by_numbers(n: int, text: str, ignore_empty_text: bool = False):
+    '''
+    На вхiд програми подається натуральне число n та деякий текст, наприклад Real.
+    Потрiбно повторити цей текст n раз в одному рядку, роздiливши копiї символом плюс "+".
+
+    Аргументи: n: int - натуральне число;
+               text: str - текст з латинських лiтер;
+               ignore_empty_text: bool = False - можливість допускати пусті тексти (аргумент text). За замовчуванням False.
+    '''
     # Validation
     validation_for_b(n, text, ignore_empty_text)
+
     # Code
     return ''.join(f'{text}+' for _ in range(n))[:-1]
 
 
 def minutes_to_time(m: int):
+    '''
+    Вiд початку доби пройшло m хвилин. Скільки годин i хвилин показує годинник на цей момент?
+
+    Аргументи: m: int - ціле число, хв.
+    '''
     # Validation
     validation_for_c(m)
+
     # Code
     hours = int(m / 60)
     minutes = m - hours * 60
@@ -29,8 +51,15 @@ def minutes_to_time(m: int):
 
 
 def number_powers_count(a: int, b: int):
+    '''
+    Потрiбно знайти i вивести кiлькiсть степенiв двiйки, що лежать на вiдрiзку [a,b]. Одиниця, як 2⁰ = 1 не враховується. (1 ≤ a ≤ b).
+
+    Аргументи: a: int - натуральне число;
+               b: int - натуральне число;
+    '''
     # Validation
     validation_for_d(a, b)
+
     # Code
     current_power = 1
     result = 0
@@ -41,6 +70,39 @@ def number_powers_count(a: int, b: int):
 
         current_power += 1
     return result
+
+
+def find_max_and_paired_number(numbers_str: str):
+    '''
+    Дано масив з n цiлих чисел. Знайти найбiльше парне число. Якщо таких чисел не iснує виведiть NO.
+
+    Аргументи: numbers_str: str - список виду тексту (наприклад: "1 3 5 7 9 11").
+
+    NOTE: Ми передаємо тільки список виду тексту, наприклад "1 3 5 7 9 11" - як дано в умові. Зокрема в умові потрібно вказувати кількість чисел у списку (n), але через можливості Python для успішного виконання коду (що було перевірено), це нам не потрібно.
+    '''
+    # Validation and Get
+    numbers = validation_for_e(numbers_str)
+
+    # Code
+    filtered_numbers = [i for i in numbers if i % 2 == 0]
+    return max(filtered_numbers) if len(filtered_numbers) > 0 else 'NO'
+
+
+def all_double_sum_numbers(m: int):
+    '''
+    Вивести всi двозначнi числа, що складаються з рiзних цифр, сума яких дорiвнює m.
+
+    Аргументи: m: int - натуральне число (0 ≤ m ≤ 20).
+    '''
+    # Validation and Get
+    validation_for_f(m)
+
+    # Code
+    results = []
+    for i in range(10, 100):
+        if int(str(i)[0]) + int(str(i)[1]) == m and str(i)[0] != str(i)[1]:
+            results.append(i)
+    return ' '.join(map(str, results)) if len(results) > 0 else 'No'
 
 
 if __name__ == '__main__':
@@ -62,4 +124,14 @@ if __name__ == '__main__':
     # D
     print('\nЗавдання D:')
     result = number_powers_count(4, 34)
+    print(result)
+
+    # E
+    print('\nЗавдання E:')
+    result = find_max_and_paired_number('2 3 5 6 1')
+    print(result)
+
+    # F
+    print('\nЗавдання F:')
+    result = all_double_sum_numbers(4)
     print(result)
--- validations.py
+++ validations.py
@@ -4,14 +4,16 @@ import sys
 # Individual Validations
 def validation_for_a(n: int):
     is_int(n)
-    if n <= 1:
+    if n <= 0:
         _exit('Аргумент n має бути більшим за 1.')
+    if n > 10**18: 
+        _exit('Аргумент n має бути не більшим за 10^18.')
 
 
 def validation_for_b(n: int, text: str, ignore_empty_text: bool):
     is_int(n)
-    if n <= 1:
-        _exit('Аргумент n має бути більшим за 1.')
+    if n <= 0:
+        _exit('Аргумент n має бути більшим за 0.')
     if not isinstance(text, str):
         _exit('Аргумент text має бути тільки типом str (текстом).')
     if not text.strip() and not ignore_empty_text:  # NOTE: або замість умови not text.strip() написати text == "", щоб можна було отримати результат "  +  +  +  ".
@@ -20,15 +22,31 @@ def validation_for_b(n: int, text: str, ignore_empty_text: bool):
 
 def validation_for_c(m: int):
     is_int(m)
-    if not 0 < m < 1440:
-        _exit('Аргумент m має бути в діапазоні від 0 до 1440.')
+    if not 0 <= m < 1440:
+        _exit('Аргумент m має бути в діапазоні від 0 (включно) до 1440.')
 
 
 def validation_for_d(a: int, b: int):
     is_int(a)
     is_int(b)
     if not 1 <= a <= b:
-        _exit('Аргументи a, b мають бути в одному рядку через промiжок (1 ≤ a ≤ b)')
+        _exit('Аргументи a, b мають бути в одному рядку через промiжок (1 ≤ a ≤ b).')
+
+
+def validation_for_e(numbers_str: str):
+    if not isinstance(numbers_str, str):
+        _exit('Аргумент numbers_str повинен бути тільки типом str, наприклад "1 3 5 7 9 11".')
+    try:
+        numbers = [int(i) for i in numbers_str.split()]
+    except ValueError as e:
+        _exit(f'Зауважте, що елементи в аргументу numbers_str повинні бути тільки цілими: "1 3 5 7 9 11". Приклад неправильних варіантів: "1 3.3 5 7 \'9\' 11". Подробиці: {e}')
+    return numbers
+
+
+def validation_for_f(m: int):
+    is_int(m)
+    if not 0 <= m <= 20:
+        _exit(f'Аргумент m має бути в діапазоні від 0 включно до 20 включно.')
 
 
 # Casual Validations
@@ -39,5 +57,5 @@ def is_int(n):
 
 # Exit
 def _exit(message):
-    print(f'Помилка:\n{message}')
-    sys.exit(1)
+    # print(f'Помилка:\n{message}')  # NOTE: Альтернатива, тоді sys.exit(1).
+    sys.exit(message)
